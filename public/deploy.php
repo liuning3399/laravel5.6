2ec365ac1ea71675bb8f18cd2ebf93308ac1d78e
liuxiang <liuxiang@globalegrow.com>
2019-05-09 10:42:04 +0800

    钩子

--- public/deploy.php
+++ public/deploy.php
@@ -4,17 +4,14 @@ $secret = "liuning";
 // 项目路径
 $path = "/data/wwwroot/www.broqiang.com";
 // 校验发送位置，正确的情况下自动拉取代码，实现自动部署
-echo shell_exec("cd {$path} && git pull");
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-print_r($_SERVER);
-print_r($signature);
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
     echo $hash;
-//  if (strcmp($signature, $hash) == 0) {
+  if (strcmp($signature, $hash) == 0) {
     echo shell_exec("cd {$path} && git pull 2>&1");
     exit();
-//  }
+  }
 }
 http_response_code(404);
 
